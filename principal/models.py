793240dedabde1f5c991d549c8e6cd7f337e6b7f
jjxp <javiergc01@gmail.com>
2016-12-30 18:29:56 +0100

    Modelo de datos, estilizada página y añadida plantilla de inicio (#7, #4, #1)
    Se ha creado el modelo de datos.
    Se ha estilizado la página para dar un formato más sencillo a la presentación del contenido.
    Se ha creado la página de inicio con un formulario (que actualmente no hace nada).
    La página de inicio muestra el número de artistas, álbumes y canciones registradas.
    Se han añadido una serie de datos de prueba temporales hasta que se tenga el dataset.

--- principal/models.py
+++ principal/models.py
@@ -1,1 +1,30 @@
 # encoding:utf-8
+from django.db import models
+
+
+class Artista(models.Model):
+    Nombre = models.CharField(max_length=50)
+    Miembros = models.CharField(max_length=140)
+    Generos = models.CharField(max_length=140)
+    
+    def __unicode__(self):
+        return self.Nombre
+    
+class Album(models.Model):
+    Nombre = models.CharField(max_length=50)
+    FechaPublicacion = models.DateField()
+    Generos = models.CharField(max_length=140)
+    ImagenPortada = models.URLField()
+    Artista = models.ForeignKey(Artista)
+    
+    def __unicode__(self):
+        return self.Nombre + " (" + str(self.FechaPublicacion) + ")"
+    
+class Cancion(models.Model):
+    Nombre = models.CharField(max_length=50)
+    EnlaceYouTube = models.URLField()
+    Generos = models.CharField(max_length=140)
+    Album = models.ForeignKey(Album)
+    
+    def __unicode__(self):
+        return self.Nombre
--- principal/views.py
+++ principal/views.py
@@ -2,6 +2,11 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
+from principal.models import Artista, Album, Cancion
+
 
 def index(request):
-    return render_to_response('index.html',{},context_instance=RequestContext(request))
+    numArtists = Artista.objects.count()
+    numAlbums = Album.objects.count()
+    numSongs = Cancion.objects.count()
+    return render_to_response('index.html',{"numArtists":numArtists,"numAlbums":numAlbums,"numSongs":numSongs},context_instance=RequestContext(request))
