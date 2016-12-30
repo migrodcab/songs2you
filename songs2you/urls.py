334eba17f0d35bb1911dcae62e52419760cdb396
jjxp <javiergc01@gmail.com>
2016-12-30 15:49:51 +0100

    Django project template

---
+++ songs2you/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$','principal.views.index'),
+    url(r'^media/(?P<path>.*)$','django.views.static.serve',
+        {'document_root':settings.MEDIA_ROOT,}
+    ),
+)
