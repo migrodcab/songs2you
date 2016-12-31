4a511fecd41db77274b07601c735a21e90bce3a7
jjxp <javiergc01@gmail.com>
2016-12-31 16:11:29 +0100

    Añadida funcionalidad de búsqueda de usuario (#5)

--- principal/forms.py
+++ principal/forms.py
@@ -1,1 +1,6 @@
 #encoding:utf-8
+from django import forms
+
+
+class SearchForm(forms.Form):
+    criteria = forms.CharField(label="Search artists, albums or songs")
--- principal/views.py
+++ principal/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
+from principal.forms import SearchForm
 from principal.models import Artista, Album, Cancion
 
 
@@ -9,4 +10,15 @@ def index(request):
     numArtists = Artista.objects.count()
     numAlbums = Album.objects.count()
     numSongs = Cancion.objects.count()
-    return render_to_response('index.html',{"numArtists":numArtists,"numAlbums":numAlbums,"numSongs":numSongs},context_instance=RequestContext(request))
+    
+    if request.method=='POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            artists = Artista.objects.filter(Nombre__contains=form.cleaned_data['criteria'])
+            albums = Album.objects.filter(Nombre__contains=form.cleaned_data['criteria'])
+            songs = Cancion.objects.filter(Nombre__contains=form.cleaned_data['criteria'])
+            return render_to_response('index.html',{'form':form,'artists':artists,'albums':albums,'songs':songs,'numArtists':numArtists,'numAlbums':numAlbums,'numSongs':numSongs},context_instance=RequestContext(request))
+    else:
+        form = SearchForm()
+    
+    return render_to_response('index.html',{"form":form,"numArtists":numArtists,"numAlbums":numAlbums,"numSongs":numSongs},context_instance=RequestContext(request))
