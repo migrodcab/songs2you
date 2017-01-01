b2d5535d8f421059e1d794bd84f53f259d63938e
jjxp <javiergc01@gmail.com>
2017-01-01 13:49:48 +0100

    Añadido reproductor y detalles de álbums
    - Se ha añadido un reproductor al hacer clic sobre una de las canciones mostradas.
    - Se ha añadido una vista de listado de detalles de un álbum.
    - En lugar de guardar la URL completa de YouTube sólo se guarda el id del vídeo.
    - Se ha comenzado la vista de detalles de un artista.

--- principal/views.py
+++ principal/views.py
@@ -1,10 +1,22 @@
 #encoding:utf-8
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template.context import RequestContext
 
 from principal.forms import SearchForm
 from principal.models import Artista, Album, Cancion
 
+
+def searchForm(request):
+    numArtists = Artista.objects.count()
+    numAlbums = Album.objects.count()
+    numSongs = Cancion.objects.count()
+    
+    form = SearchForm(request.POST)
+    if form.is_valid():
+        artists = Artista.objects.filter(Nombre__contains=form.cleaned_data['criteria'])
+        albums = Album.objects.filter(Nombre__contains=form.cleaned_data['criteria'])
+        songs = Cancion.objects.filter(Nombre__contains=form.cleaned_data['criteria'])
+        return render_to_response('index.html',{'form':form,'artists':artists,'albums':albums,'songs':songs,'numArtists':numArtists,'numAlbums':numAlbums,'numSongs':numSongs},context_instance=RequestContext(request))
 
 def index(request):
     numArtists = Artista.objects.count()
@@ -12,13 +24,24 @@ def index(request):
     numSongs = Cancion.objects.count()
     
     if request.method=='POST':
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            artists = Artista.objects.filter(Nombre__contains=form.cleaned_data['criteria'])
-            albums = Album.objects.filter(Nombre__contains=form.cleaned_data['criteria'])
-            songs = Cancion.objects.filter(Nombre__contains=form.cleaned_data['criteria'])
-            return render_to_response('index.html',{'form':form,'artists':artists,'albums':albums,'songs':songs,'numArtists':numArtists,'numAlbums':numAlbums,'numSongs':numSongs},context_instance=RequestContext(request))
+        return searchForm(request)
     else:
         form = SearchForm()
     
     return render_to_response('index.html',{"form":form,"numArtists":numArtists,"numAlbums":numAlbums,"numSongs":numSongs},context_instance=RequestContext(request))
+
+def displayAlbum(request,albumId):
+    album = get_object_or_404(Album, pk=albumId)
+    songs = Cancion.objects.filter(Album=album)
+    
+    if request.method=='POST':
+        return searchForm(request)
+    else:
+        form = SearchForm()
+    
+    return render_to_response('album.html',{'form':form,'album':album,'songs':songs},context_instance=RequestContext(request))
+
+def displayArtist(request,artistId):
+    artist = get_object_or_404(Artista, pk=artistId)
+    
+    return render_to_response('artist.html',{'artist':artist},context_instance=RequestContext(request))
