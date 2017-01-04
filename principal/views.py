534976f646593dd364a2e4226aeb1342d1272567
migrodcab <miguel.rc95@gmail.com>
2017-01-04 10:30:11 +0100

    Vista de todas las canciones con paginacion

--- principal/views.py
+++ principal/views.py
@@ -1,6 +1,7 @@
 #encoding:utf-8
-from django.shortcuts import render_to_response, get_object_or_404
+from django.shortcuts import render_to_response, get_object_or_404, render
 from django.template.context import RequestContext
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from principal.forms import SearchForm
 from principal.models import Artista, Album, Cancion
@@ -29,6 +30,26 @@ def index(request):
         form = SearchForm()
     
     return render_to_response('index.html',{"form":form,"numArtists":numArtists,"numAlbums":numAlbums,"numSongs":numSongs},context_instance=RequestContext(request))
+
+def canciones(request):
+    canciones = Cancion.objects.all()
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(canciones, 4)
+    try:
+        data = paginator.page(page)
+    except PageNotAnInteger:
+        data = paginator.page(1)
+    except EmptyPage:
+        data = paginator.page(paginator.num_pages)
+
+    #return render(request, 'canciones.html', { 'data': data })
+    if request.method=='POST':
+        return searchForm(request)
+    else:
+        form = SearchForm()
+    
+    return render_to_response('canciones.html',{'form':form,'data':data},context_instance=RequestContext(request))
 
 def displayAlbum(request,albumId):
     album = get_object_or_404(Album, pk=albumId)
