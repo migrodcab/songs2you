0d29a90b91d95d7314e98e85b3a1ea1c4e054814
Carlos Alberto Mata Gil <CeuxDruman@gmail.com>
2017-01-08 20:29:34 +0100

    Added README.txt file generation in datasetScraping
    datasetScraping.py now generates an explanatory README.txt file with a
    description, data statistics, files and data format information.

--- datasetScraping.py
+++ datasetScraping.py
@@ -25,18 +25,36 @@ from bs4 import BeautifulSoup
 
 __author__ = "Carlos Alberto Mata Gil"
 
-__version__ = "2.0"
+__version__ = "3.0"
 __email__ = "carmatgil@alum.us.es"
 __status__ = "Production"
 
 def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPerGenre=None, maxAlbumsPerArtist=None, maxSongsPerAlbum=None):
     """Args:
-        allowedGenresNames: An optional string or tuple of strings with the name or names of all genres you want to scrap. If not passed, it'll scrape all genres by default.
-        finishSoundNotification: An optional boolean. If True, you will receive a 'Beep' sound notification when the web scraping finish. If not passed, it'll take False as default value. Note: This feature might work only on Windows devices.
-        maxArtistsPerGenre: An optional integer number that represents the maximum number of artists that will be scraped for each genre. If not passed, it'll scrape all artists by default.
-        maxAlbumsPerArtist: An optional integer number that represents the maximum number of albums that will be scraped for each artist. If not passed, it'll scrape all albums by default.
-        maxSongsPerAlbum: An optional integer number that represents the maximum number of songs that will be scraped for each album. If not passed, it'll scrape all songs by default.
+        allowedGenresNames: An optional string or tuple of strings with the name or names of all genres you want to scrap. 
+            If not passed, it'll scrape all genres by default.
+        finishSoundNotification: An optional boolean. 
+            If True, you will receive a 'Beep' sound notification when the web scraping finish. 
+            If not passed, it'll take False as default value. 
+            Note: This feature might work only on Windows devices.
+        maxArtistsPerGenre: An optional integer number that represents the maximum number of artists that will be scraped for each genre. 
+            If not passed, it'll scrape all artists by default.
+        maxAlbumsPerArtist: An optional integer number that represents the maximum number of albums that will be scraped for each artist. 
+            If not passed, it'll scrape all albums by default.
+        maxSongsPerAlbum: An optional integer number that represents the maximum number of songs that will be scraped for each album. 
+            If not passed, it'll scrape all songs by default.
+        
+    Returns:
+        A tuple of integer numbers representing the number of Genres analyzed and the number of Artists, Albums and Songs stored, in that order.
+        For example:
+
+        (21, 925, 2675, 26049)
     """
+    
+    numberOfGenres = 0
+    numberOfArtists = 0
+    numberOfAlbums = 0
+    numberOfSongs = 0
     
     if not (allowedGenresNames is None or isinstance(allowedGenresNames, basestring) or (isinstance(allowedGenresNames, tuple) and all(isinstance(genreName, basestring) for genreName in allowedGenresNames) ) ):
         print "### [INFO] Skipping 'allowedGenresNames' argument"
@@ -85,7 +103,7 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
         artistsFile.close()
         albumsFile.close()
         songsFile.close()
-        return
+        return (numberOfGenres, numberOfArtists, numberOfAlbums, numberOfSongs)
     genresSoup = BeautifulSoup(html, 'html.parser')
     
     print "## [STARTED] Searching for Genres"
@@ -93,7 +111,9 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
     
     genres = genresSoup.findAll('div', {"class": "genre"})
     
-    print "## [DONE] (" + str(round(time.time() - start_time,3)) + " secs) Searching for Genres: " + str(len(genres)) + " genres found."
+    numberOfGenres = len(genres)
+    
+    print "## [DONE] (" + str(round(time.time() - start_time,3)) + " secs) Searching for Genres: " + str(numberOfGenres) + " genres found."
     
     for genre in genres:
         genreName = genre("h2")[0]("a")[0].getText()
@@ -114,7 +134,7 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
             artistsFile.close()
             albumsFile.close()
             songsFile.close()
-            return
+            return (numberOfGenres, numberOfArtists, numberOfAlbums, numberOfSongs)
         artistsSoup = BeautifulSoup(html, 'html.parser')
         
         artistsContainer = artistsSoup.find("div", {"class": "artist-highlights-container"})
@@ -142,7 +162,7 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
                 artistsFile.close()
                 albumsFile.close()
                 songsFile.close()
-                return
+                return (numberOfGenres, numberOfArtists, numberOfAlbums, numberOfSongs)
             artistSoup = BeautifulSoup(html, 'html.parser')
             
             artistMembersContainer = artistSoup.find("div", {"class": "group-members"})
@@ -188,6 +208,8 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
                 artistList.append('')
             
             artistsWriter.writerow(artistList)
+            numberOfArtists += 1
+            
             print "# [INFO] Analyzing artist '" + artistList[1] + "'"
             
             req = urllib2.Request("http://www.allmusic.com/artist/MN"+artistId+"/discography", headers=hdr)
@@ -198,7 +220,7 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
                 artistsFile.close()
                 albumsFile.close()
                 songsFile.close()
-                return
+                return (numberOfGenres, numberOfArtists, numberOfAlbums, numberOfSongs)
             albumsSoup = BeautifulSoup(html, 'html.parser')
             
             try:
@@ -236,7 +258,7 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
                     artistsFile.close()
                     albumsFile.close()
                     songsFile.close()
-                    return
+                    return (numberOfGenres, numberOfArtists, numberOfAlbums, numberOfSongs)
                 albumSoup = BeautifulSoup(html, 'html.parser')
                  
                 albumGenresContainer = albumSoup.find("div", {"class": "genre"})
@@ -285,6 +307,7 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
                     albumList.append('')
                      
                 albumsWriter.writerow(albumList)
+                numberOfAlbums += 1
                 
                 req = urllib2.Request("http://www.allmusic.com/album/MW"+albumId, headers=hdr)
                 try:
@@ -294,7 +317,7 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
                     artistsFile.close()
                     albumsFile.close()
                     songsFile.close()
-                    return
+                    return (numberOfGenres, numberOfArtists, numberOfAlbums, numberOfSongs)
                 songsSoup = BeautifulSoup(html, 'html.parser')
                   
                 songs = songsSoup.findAll("tr", {"class": "track"})
@@ -332,7 +355,7 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
                         artistsFile.close()
                         albumsFile.close()
                         songsFile.close()
-                        return
+                        return (numberOfGenres, numberOfArtists, numberOfAlbums, numberOfSongs)
                     songSoup = BeautifulSoup(html, 'html.parser')
                      
                     songGenresContainer = songSoup.find("div", {"class": "genre"})
@@ -362,11 +385,12 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
                         response = urllib2.urlopen(req)
                         html = response.read()
                         songYouTubeVideoId = re.findall(r'"videoId": "(.+)"',html)[0]
-                        songList.append(songYouTubeVideoId)                                                                                 # youtube video id
+                        songList.append(songYouTubeVideoId)                                                                             # youtube video id
                     except:
                         songList.append("?")
                     
                     songsWriter.writerow(songList)
+                    numberOfSongs += 1
         
         print "## [DONE] (" + str(round(time.time() - start_time,3)) + " secs) Analyzing genre '" + genreName + "'"
     
@@ -380,6 +404,116 @@ def scrape(allowedGenresNames=None, finishSoundNotification=False, maxArtistsPer
         Freq = 2500 # Frequency in Hertz
         Dur = 500 # Duration in ms
         winsound.Beep(Freq,Dur)
+        
+    return (numberOfGenres, numberOfArtists, numberOfAlbums, numberOfSongs)
+        
+def generateReadmeFile(dataStatistics):
+    """Generates a README.txt file with information about the dataset .csv files
+    
+    This function generates a README.txt on dataset folder in which it is
+    explained where were all data retreived from and what's the meaning of
+    each column for each .csv file.
+    
+    Args:
+        dataStatistics: A tuple of integer numbers representing the number of Genres analyzed and the number of Artists, Albums and Songs stored, in that order.
+    """
+    
+    if not os.path.exists("dataset"):
+        os.makedirs("dataset")
+        
+    readmeFile  = open('dataset/README.txt', "w")
+    
+    readmeContent = """
+=======
+dataset
+=======
+
+-------
+Version
+-------
+
+Version {version} (2017-01-08)
+
+-----------
+Description
+-----------
+
+    This dataset contains music artists, albums and songs information 
+    retreived from using web scraping on http://www.allmusic.com
+
+    The dataset was created for a college work for the subject
+    'Acceso Inteligente a la Información' of the department
+    'Lenguajes y Sistemas Informáticos' on the degree 'Ingeniería 
+    Informática del Software' of the University of Seville, Spain.
+
+---------------
+Data statistics
+---------------
+
+   genres* {numberOfGenres}
+   artists {numberOfArtists}
+    albums {numberOfAlbums}
+     songs {numberOfSongs}
+     
+   *genres just represent the number of analyzed genres, but 
+   there isn't an specific .csv file for storing their 
+   information.
+
+-----
+Files
+-----
+
+   * artists.csv
+   
+        This file contains information about music artists of every analyzed genre.
+        
+   * albums.csv
+   
+        This file contains information about albums of every analyzed artists.
+        
+   * songs.csv
+   
+        This file contains information about songs of every analyzed albums.
+    
+-----------
+Data format
+-----------
+
+   The data is formatted one entry per line as follows (semicolon separated, ";"):
+
+   * artists.csv
+   
+        id;name;members;genre;styles;photo
+
+        Example:
+        0000074616;Borbetomagus;Donald Dietrich, Donald Miller, Jim Sauter;Avant-Garde, Jazz;Free Improvisation, Fusion, Noise;http://cps-static.rovicorp.com/3/JPG_400/MI0003/150/MI0003150663.jpg?partner=allrovi.com
+
+   * albums.csv
+ 
+        artistId;id;name;label;genre;styles;releaseDate;coverImage;duration
+        0000664817;0000650124;Let It Loose;Epic;Pop/Rock;Dance-Pop, Latin Pop, Contemporary Pop/Rock;June 2, 1987;http://cdn-s3.allmusic.com/release-covers/500/0000/280/0000280376.jpg;38:09
+ 
+   * songs.csv
+   
+        albumId;id;trackNumber;name;duration;genre;styles;youtubeVideoId
+        0000653824;0005944471;3;Say;3:41;Latin, Jazz, International;Latin Jazz, World Fusion, Brazilian Traditions;TBOvPBvQP3o
+
+"""
+    
+    context = {
+        "numberOfGenres": dataStatistics[0], 
+        "numberOfArtists": dataStatistics[1],
+        "numberOfAlbums": dataStatistics[2],
+        "numberOfSongs": dataStatistics[3],
+        "version": __version__
+    }
+    
+    readmeFile.write(readmeContent.format(**context))
+    
+    readmeFile.close()
+    
+    print "### [INFO] README.txt generated"
 
 if __name__ == '__main__':
-    scrape()
+    dataStatistics = scrape()
+    generateReadmeFile(dataStatistics)
